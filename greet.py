10598cc030b4b0f27e781e1a3f8da966de5898c1
VihaanJoshi1 <127643130+VihaanJoshi1@users.noreply.github.com>
2023-03-22 16:07:22 +0530

    Update greet.py

--- greet.py
+++ greet.py
@@ -3,11 +3,14 @@ def greet(language):
     if language = 1:
         print(Guten morgen!)
 
-    if language = 2:
+        elif language = 2:
         print(buen día!)
 
-    if language = 3:
+        elif language = 3:
         print(good morning!)
+        
+        else:
+            print("wrong language dude...")
 
 greeting = greet(1)
     print(greeting)
